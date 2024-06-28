d1818dedb940ea3672a2a139d88ca238d56d0701
cl0wnfoon <87553151+cl0wnfoon@users.noreply.github.com>
2024-06-28 12:56:39 -0500

    Update ScriptScanner.lua

--- modules/ScriptScanner.lua
+++ modules/ScriptScanner.lua
@@ -9,30 +9,4 @@ local requiredMethods = {
     ["getScriptClosure"] = true,
     ["isXClosure"] = true
 }
-
-local function scan(query)
-    local scripts = {}
-    query = query or ""
-
-    for _i, v in pairs(getGc()) do
-        if type(v) == "function" and not isXClosure(v) then
-            local script = rawget(getfenv(v), "script")
-
-            if typeof(script) == "Instance" and 
-                not scripts[script] and 
-                script:IsA("LocalScript") and 
-                script.Name:lower():find(query) and
-                getScriptClosure(script) and
-                pcall(function() getsenv(script) end)
-            then
-                scripts[script] = LocalScript.new(script)
-            end
-        end
-    end
-
-    return scripts
-end
-
-ScriptScanner.RequiredMethods = requiredMethods
-ScriptScanner.Scan = scan
 return ScriptScanner
